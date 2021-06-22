684994801c3fba845ca14e6c955b3033df8bb673
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2021-06-22 14:14:35 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import AppState from './context/AppState';
-import { QueryClient, QueryClientProvider } from 'react-query';
-
-const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient} contextSharing={true}>
       <AppState>
         <App />
       </AppState>
-    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
